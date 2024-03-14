5167e11722e2adc90595044635a24c719a5fc48f
Tangatarov Nurzhan <tangatarovnurzan2@gmail.com>
2024-03-14 18:35:45 +0600

    Added user role transmition

--- src/pages/TransferPage.tsx
+++ src/pages/TransferPage.tsx
@@ -1,23 +1,52 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
-// import { setUser, UserState } from "../store/slices/userSlice.ts";
+import { setUser } from "../store/slices/userSlice.ts";
 import { useEffect } from "react";
+import { useAppSelector } from "../store/hooks.ts";
 
 const TransferPage = () => {
+  const user = useAppSelector((state) => state.user);
   const [searchParams] = useSearchParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log(window.Telegram.WebApp)
+    const postData = {
+      telegram_id: window.Telegram.WebApp?.initDataUnsafe.user.id,
+      telegram_fullname: "",
+      promo: "",
+    };
+
+    fetch("https://back.pizzeria-almaty.kz/auth/register/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        dispatch(setUser(data));
+      })
+      .catch((error) => {
+        console.error("Error during registration:", error);
+      });
   }, [dispatch, navigate, searchParams]);
 
+  useEffect(() => {
+    if (user.role === "client") {
+      navigate("/menu");
+    } else if (user.role === "manager") {
+      navigate("/orders");
+    }
+  }, [navigate, user]);
 
-  return (
-    <div>
-      {JSON.stringify(window.Telegram.WebApp, null, 10)}
-    </div>
-  );
+  return <div>{JSON.stringify(window.Telegram.WebApp, null, 10)}</div>;
 };
 
 export default TransferPage;
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./slices/counterSlice.ts";
 import mainReducer from "./slices/mainSlice.ts";
 import orderSlice from "./slices/orderSlice.ts";
+import userSlice from "./slices/userSlice.ts";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     main: mainReducer,
     order: orderSlice,
+    user: userSlice,
   },
 });
 
