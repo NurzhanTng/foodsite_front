1be2f2360080cfdffad55334a6812efa3b8427ca
Tangatarov Nurzhan <tangatarovnurzan2@gmail.com>
2024-03-17 15:59:15 +0600

    test3

--- src/pages/TransferPage/ui/TransferPage.tsx
+++ src/pages/TransferPage/ui/TransferPage.tsx
@@ -1,7 +1,10 @@
 // import useMainHook from "../hooks/useMainHook.ts";
 // import { useEffect } from "react";
 
+import { useAppSelector } from "../../../store/hooks.ts";
+
 const TransferPage = () => {
+  const state = useAppSelector((state) => state);
   // const { user, navigate } = useMainHook();
 
   // useEffect(() => {
@@ -12,7 +15,12 @@ const TransferPage = () => {
   //   }
   // }, [navigate, user]);
 
-  return <div>{JSON.stringify(window.Telegram.WebApp.initDataUnsafe, null, 10)}</div>;
+  return (<div className="flex flex-col gap-5">
+    <p>window.Telegram.WebApp.initDataUnsafe: {JSON.stringify(window.Telegram.WebApp.initDataUnsafe, null, 10)}</p>
+    <p>User: {JSON.stringify(state.user)}</p>
+    <p>Cart: {JSON.stringify(state.main.cart)}</p>
+    {/*<p>User: {JSON.stringify(state.user)}</p>*/}
+  </div>);
 };
 
 export default TransferPage;
