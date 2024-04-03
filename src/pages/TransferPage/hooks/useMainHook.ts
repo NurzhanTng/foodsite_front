284397691cb4deabc4ad9c7f35ff157272f0a332
Tangatarov Nurzhan <tangatarovnurzan2@gmail.com>
2024-04-03 22:22:49 +0600

    test transfer page

--- src/pages/TransferPage/hooks/useMainHook.ts
+++ src/pages/TransferPage/hooks/useMainHook.ts
@@ -1,18 +1,21 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 
 import { setUser, UserState } from "../../../store/slices/userSlice.ts";
-import { useAppSelector } from "../../../store/hooks.ts";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks.ts";
 import { setUserData } from "../../../store/slices/orderSlice.ts";
+import { fetchCategories } from "../../../store/slices/mainSlice.ts";
+import { fetchCompanies } from "../../../store/slices/companySlice.ts";
+import { fetchDeliveries, fetchOrders } from "../../../store/slices/managerSlice.ts";
 
 const useMainHook = () => {
   const user = useAppSelector((state) => state.user);
   const [searchParams] = useSearchParams();
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
+    console.log("--- useMainHook ---")
     const bot_id = window.Telegram.WebApp?.initDataUnsafe?.user?.id;
     const params_id = searchParams.get("telegram_id");
     const telegram_id = bot_id ? bot_id : params_id;
@@ -39,10 +42,18 @@ const useMainHook = () => {
         return response.json();
       })
       .then((data: UserState) => {
-        console.log("user ", data);
         // data.kaspi_phone = data.phone;
+        dispatch(fetchCategories());
+        dispatch(fetchCompanies());
         dispatch(setUser(data));
         dispatch(setUserData(data));
+        if (data.role === "client") {
+          navigate("/menu");
+        } else if (data.role === "manager") {
+          dispatch(fetchOrders());
+          dispatch(fetchDeliveries());
+          navigate("/orders");
+        }
       })
       .catch((error) => {
         console.error("Error during registration:", error);
--- src/pages/TransferPage/ui/TransferPage.tsx
+++ src/pages/TransferPage/ui/TransferPage.tsx
@@ -1,30 +1,7 @@
 import useMainHook from "../hooks/useMainHook.ts";
-import { useAppDispatch, useAppSelector } from "../../../store/hooks.ts";
-import { useEffect } from "react";
-import { fetchCategories } from "../../../store/slices/mainSlice.ts";
-import { fetchCompanies } from "../../../store/slices/companySlice.ts";
-import {
-  fetchDeliveries,
-  fetchOrders,
-} from "../../../store/slices/managerSlice.ts";
 
 const TransferPage = () => {
-  const { user, navigate } = useMainHook();
-  const orders = useAppSelector((state) => state.manager.orders);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(fetchCategories());
-    dispatch(fetchCompanies());
-    console.log("User Role: ", user.role);
-    if (user.role === "client") {
-      navigate("/menu");
-    } else if (user.role === "manager") {
-      dispatch(fetchOrders());
-      dispatch(fetchDeliveries());
-      navigate("/orders");
-    }
-  }, [dispatch, navigate, orders, user]);
+  useMainHook();
 
   return (
     <div className="flex flex-col gap-5">
