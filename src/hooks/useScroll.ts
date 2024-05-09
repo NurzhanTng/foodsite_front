669868cc45bfe7c5ac763b40c946b2190d33e501
Tangatarov Nurzhan <tangatarovnurzan2@gmail.com>
2024-05-09 18:49:40 +0500

    test

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -4,18 +4,19 @@ const useScroll = (offset: number = 0) => {
   const ref = useRef<HTMLInputElement>(null);
 
   function alertData(bounding: number) {
-    alert("Screen height: " + window.screen.height);
-    alert("Window height: " + window.document.documentElement.scrollHeight);
-    alert("Visual viewport height: " + window.visualViewport?.height);
-    alert("2 * window.scrollY: " + 2 * window.scrollY);
-    alert(
-      "window.visualViewport?.height / 2: " +
-        (window.visualViewport?.height
-          ? window.visualViewport?.height / 2
-          : 300),
-    );
-    alert("ref.current.getBoundingClientRect().top: " + bounding);
-    alert("offset: " + offset);
+    console.log(bounding);
+    // alert("Screen height: " + window.screen.height);
+    // alert("Window height: " + window.document.documentElement.scrollHeight);
+    // alert("Visual viewport height: " + window.visualViewport?.height);
+    // alert("2 * window.scrollY: " + 2 * window.scrollY);
+    // alert(
+    //   "window.visualViewport?.height / 2: " +
+    //     (window.visualViewport?.height
+    //       ? window.visualViewport?.height / 2
+    //       : 300),
+    // );
+    // alert("ref.current.getBoundingClientRect().top: " + bounding);
+    // alert("offset: " + offset);
   }
 
   const scrollToElement = useCallback((new_offset?: number) => {
@@ -25,10 +26,10 @@ const useScroll = (offset: number = 0) => {
         alertData(ref.current.getBoundingClientRect().top);
         const yOffset =
           window.scrollY +
-          // window.innerHeight -
+          window.innerHeight -
           (window.visualViewport?.height
             ? window.visualViewport?.height / 2
-            : 300) -
+            : 0) -
           ref.current.getBoundingClientRect().top;
         // offset;
         window.scrollTo({ top: yOffset, behavior: "smooth" });
