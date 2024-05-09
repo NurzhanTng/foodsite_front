f18f16d4cc15cc04fc0a3c491d76d2e28c5469e2
Tangatarov Nurzhan <tangatarovnurzan2@gmail.com>
2024-05-09 18:33:38 +0500

    test

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -3,16 +3,25 @@ import { useCallback, useRef } from "react";
 const useScroll = (offset: number = 0) => {
   const ref = useRef<HTMLInputElement>(null);
 
-  function alertData() {
+  function alertData(bounding: number) {
     alert("Screen height: " + window.screen.height);
     alert("Window height: " + window.document.documentElement.scrollHeight);
     alert("Visual viewport height: " + window.visualViewport?.height);
+    alert("2 * window.scrollY: " + 2 * window.scrollY);
+    alert(
+      "window.visualViewport?.height / 2: " +
+        (window.visualViewport?.height
+          ? window.visualViewport?.height / 2
+          : 300),
+    );
+    alert("ref.current.getBoundingClientRect().top: " + bounding);
+    alert("offset: " + offset);
   }
 
   const scrollToElement = useCallback((new_offset?: number) => {
     function scroll(offset: number) {
       if (ref.current) {
-        alertData();
+        alertData(ref.current.getBoundingClientRect().top);
         const yOffset =
           2 * window.scrollY +
           (window.visualViewport?.height
