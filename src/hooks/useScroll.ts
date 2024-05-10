0751b2c661c319266ba7525f7d6045fca846b43b
Tangatarov Nurzhan <tangatarovnurzan2@gmail.com>
2024-05-10 13:27:12 +0500

    test

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,8 +1,9 @@
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useRef } from "react";
+
+let it = 0;
 
 const useScroll = (offset: number = 0) => {
   const ref = useRef<HTMLInputElement>(null);
-  const [it, setIt] = useState(0);
 
   const scrollToElement = useCallback((new_offset?: number) => {
     function scroll(offset: number) {
@@ -16,15 +17,13 @@ const useScroll = (offset: number = 0) => {
         const yOffset = input + (direction * height) / 2 + offset;
 
         window.scrollTo({ top: start + yOffset, behavior: "smooth" });
+        setTimeout(() => alert(200 + (it - 1) * 50), 500);
+        it = it + 1;
       }
     }
 
     setTimeout(
-      () => {
-        scroll(new_offset === undefined ? offset : new_offset);
-        setIt((it) => it + 1);
-        setTimeout(() => alert(200 + it * 50), 500);
-      },
+      () => scroll(new_offset === undefined ? offset : new_offset),
       200 + it * 50,
     );
   }, []);
