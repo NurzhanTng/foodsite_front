9c71ef52f0f7648948457c4dabeecbdc8dda49a7
Tangatarov Nurzhan <tangatarovnurzan2@gmail.com>
2024-05-14 21:16:08 +0500

    test

--- src/features/BottomSlide/ui/BottomSlide.tsx
+++ src/features/BottomSlide/ui/BottomSlide.tsx
@@ -4,7 +4,6 @@ import { twMerge } from "tailwind-merge";
 type BottomSlideProps = React.ButtonHTMLAttributes<HTMLDivElement> & {
   stage: 0 | 1 | 2;
   setStage: (stage: 0 | 1 | 2) => void;
-  update: boolean;
 };
 
 const tg = window.Telegram.WebApp;
@@ -14,23 +13,15 @@ const BottomSlide = ({
   children,
   setStage,
   stage,
-  update,
 }: BottomSlideProps) => {
   const [height, setHeight] = useState<number>(getHeight(stage));
-  const [margin, setMargin] = useState<number>(getTopMargin(stage));
   const [active, setActive] = useState(false);
   const [startHeight, setStartHeight] = useState(0);
-  const [startMargin, setStartMargin] = useState(0);
   const [startCoords, setStartCoords] = useState(0);
 
   useEffect(() => {
     console.log(stage);
   }, [stage]);
-
-  useEffect(() => {
-    setHeight(getHeight(stage));
-    setMargin(getTopMargin(stage));
-  }, [update]);
 
   useEffect(() => {
     setHeight(getHeight(stage));
@@ -43,28 +34,10 @@ const BottomSlide = ({
     return [160, windowHeight * 0.5, windowHeight * 0.9][stage];
   }
 
-  function getTopMargin(stage: 0 | 1 | 2) {
-    const windowHeight = window.visualViewport?.height
-      ? window.visualViewport.height
-      : window.innerHeight;
-    return [windowHeight - 160, windowHeight * 0.5, windowHeight * 0.1][stage];
-  }
-
   const handleTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
     console.log("touch start");
     setStartCoords(event.touches[0].clientY);
     setStartHeight(height);
-    setStartMargin(margin);
-    setActive(true);
-  };
-
-  const handleMouseDown = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-  ) => {
-    console.log("mouse down", height, margin);
-    setStartCoords(event.clientY);
-    setStartHeight(height);
-    setStartMargin(margin);
     setActive(true);
   };
 
@@ -90,7 +63,6 @@ const BottomSlide = ({
 
   useEffect(() => {
     setHeight(getHeight(stage));
-    setMargin(getTopMargin(stage));
   }, [stage]);
 
   useEffect(() => {
@@ -100,53 +72,24 @@ const BottomSlide = ({
       if (!tg.isExpanded) tg.expand;
       const deltaY = event.touches[0].clientY - startCoords;
       const newHeight = startHeight - deltaY;
-      console.log("move", startMargin + deltaY, newHeight);
       setHeight(Math.min(Math.max(newHeight, getHeight(0)), getHeight(2)));
-      setMargin(
-        Math.max(
-          Math.min(startMargin + deltaY, getTopMargin(0)),
-          getTopMargin(2),
-        ),
-      );
     };
-
-    const handleMouseMove = (event: MouseEvent) => {
-      event.preventDefault();
-      // if (!active) return;
-      if (!tg.isExpanded) tg.expand;
-      const deltaY = event.clientY - startCoords;
-      const newHeight = startHeight - deltaY;
-      console.log("move", startMargin + deltaY, newHeight);
-      setHeight(Math.min(Math.max(newHeight, getHeight(0)), getHeight(2)));
-      setMargin(
-        Math.max(
-          Math.min(startMargin + deltaY, getTopMargin(0)),
-          getTopMargin(2),
-        ),
-      );
-    };
-
     window.addEventListener("touchmove", handleTouchMove, { passive: false });
-    window.addEventListener("mousemove", handleMouseMove);
-    window.addEventListener("mouseup", handleTouchEnd);
 
     return () => {
       window.removeEventListener("touchmove", handleTouchMove);
-      window.removeEventListener("mousemove", handleMouseMove);
-      window.removeEventListener("mouseup", handleTouchEnd);
     };
   }, [startCoords, startHeight]);
 
   return (
     <div
       className={twMerge(
-        `${startCoords === 0 ? "duration-300" : ""} transition-height fixed left-0 z-10 w-full transition ease-in-out`,
+        `${startCoords === 0 ? "duration-300" : ""} transition-height fixed bottom-0 left-0 z-10 w-full transition ease-in-out`,
         className,
       )}
-      style={{ height: `${height}px`, top: `${margin}px` }}
+      style={{ height: `${height}px` }}
       onTouchStart={handleTouchStart}
       onTouchEnd={handleTouchEnd}
-      onClick={handleMouseDown}
     >
       {children}
     </div>
--- src/pages/DeliveryPage/ui/SlideMenu.tsx
+++ src/pages/DeliveryPage/ui/SlideMenu.tsx
@@ -27,7 +27,6 @@ const SlideMenu = ({
 }: SlideMenuProps) => {
   const {
     ref,
-    update,
     stage,
     setStage,
     isSearchActive,
@@ -52,7 +51,6 @@ const SlideMenu = ({
       className="z-0 overflow-y-auto bg-bgColor px-[20px] pb-[70px]"
       stage={stage}
       setStage={setStage}
-      update={update}
     >
       <div className="absolute left-[50%] top-[7px] h-[3px] w-[50px] translate-x-[-50%] rounded-[90px] bg-button "></div>
 
