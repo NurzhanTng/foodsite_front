8c0a1b16ed0de88dd7a550228a94bd71ac0bdee1
Tangatarov Nurzhan <tangatarovnurzan2@gmail.com>
2024-05-19 13:02:29 +0500

    test

--- src/pages/PhotoPage/ui/PhotoPage.tsx
+++ src/pages/PhotoPage/ui/PhotoPage.tsx
@@ -10,7 +10,7 @@ const PhotoPage: React.FC = () => {
     const initCamera = async () => {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
+          video: { facingMode: "user" },
         });
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
